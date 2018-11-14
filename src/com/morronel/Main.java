ffe7fbdd743e94a1cec1abb9fd3d4e1e689efe22
Morronel <zahar.kogut@gmail.com>
2018-11-14 06:45:27 +0200

    Prototype is completed!

--- src/com/morronel/Main.java
+++ src/com/morronel/Main.java
@@ -9,26 +9,13 @@ public class Main {
 
     public static void main(String[] args) {
         String path;
-        List<List<Character>> listOfLists = new ArrayList<>();
-        List<Character> characterArrayList = new ArrayList<>();
-        int cores = Runtime.getRuntime().availableProcessors();
+        List<List<Character>> listOfLists;
+        List<Character> characterArrayList;
+        int cores;
 
+        cores = Runtime.getRuntime().availableProcessors();
         path = "/home/uucyc/Desktop/gli.zip";
-
-        //adding lowercase chars
-        for (int i = 97; i < 123; i++) {
-            characterArrayList.add((char) i);
-        }
-
-        //adding uppercase chars
-        for (int i = 65; i < 91; i++) {
-            characterArrayList.add((char) i);
-        }
-
-        //adding numbers
-        for (int i = 48; i < 58; i++) {
-            characterArrayList.add((char) i);
-        }
+        characterArrayList = Utils.characterListBuilder("-n");
 
         if (cores > characterArrayList.size()) {
             cores = characterArrayList.size();
--- src/com/morronel/Utils.java
+++ src/com/morronel/Utils.java
@@ -9,6 +9,90 @@ public class Utils {
 
     }
 
+    public static List<Character> characterListBuilder(){
+        List<Character> characterArrayList = new ArrayList<>();
+
+        //adding lowercase chars
+        for (int i = 97; i < 123; i++) {
+            characterArrayList.add((char) i);
+        }
+
+        //adding special symbols
+        for (int i = 32; i < 48; i++) {
+            characterArrayList.add((char) i);
+        }
+
+        for (int i = 58; i < 65; i++) {
+            characterArrayList.add((char) i);
+        }
+
+        for (int i = 91; i < 96; i++) {
+            characterArrayList.add((char) i);
+        }
+
+        for (int i = 123; i < 126; i++) {
+            characterArrayList.add((char) i);
+        }
+
+        //adding uppercase chars
+        for (int i = 65; i < 91; i++) {
+            characterArrayList.add((char) i);
+        }
+
+        //adding numbers
+        for (int i = 48; i < 58; i++) {
+            characterArrayList.add((char) i);
+        }
+
+        return characterArrayList;
+    }
+
+    public static List<Character> characterListBuilder(String params){
+        List<Character> characterArrayList = new ArrayList<>();
+
+        if (params.contains("-l") || params.contains("-c") || params.contains("-a")) {
+            //adding lowercase chars
+            for (int i = 97; i < 123; i++) {
+                characterArrayList.add((char) i);
+            }
+        }
+
+        if (params.contains("-l") || params.contains("-C") || params.contains("-a")) {
+            //adding uppercase chars
+            for (int i = 65; i < 91; i++) {
+                characterArrayList.add((char) i);
+            }
+        }
+
+        if (params.contains("-n") || params.contains("-a")) {
+            //adding numbers
+            for (int i = 48; i < 58; i++) {
+                characterArrayList.add((char) i);
+            }
+        }
+
+        if (params.contains("-s") || params.contains("-a")) {
+            //adding special symbols
+            for (int i = 32; i < 48; i++) {
+                characterArrayList.add((char) i);
+            }
+
+            for (int i = 58; i < 65; i++) {
+                characterArrayList.add((char) i);
+            }
+
+            for (int i = 91; i < 96; i++) {
+                characterArrayList.add((char) i);
+            }
+
+            for (int i = 123; i < 126; i++) {
+                characterArrayList.add((char) i);
+            }
+        }
+
+        return characterArrayList;
+    }
+
     public static List<List<Character>> splitArrayByThreadAmount(List<Character> origin, int threadCount){
         List<List<Character>> result = new ArrayList<>(threadCount);
         int len = origin.size();
